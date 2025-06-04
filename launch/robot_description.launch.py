da00b3dfe7e37f2538b3964e57f36fd881d955e4
Luc Fabresse <luc.fabresse@imt-nord-europe.fr>
2025-06-04 16:08:26 +0200

    update to ROS2

---
+++ launch/robot_description.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    urdf_file = os.path.join(get_package_share_directory('swd_starter_kit_description'), 'urdf', 'swd_starter_kit.urdf')
+
+    with open(urdf_file, 'r') as infp:
+        robot_desc = infp.read()
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_desc}]
+        )
+    ])
